07c35ccbd58b32f7bc8b84104c8945a08666c5cd
Prabhat020 <kumar.prabhat23343@jeckukas.org.in>
2025-09-29 16:17:59 +0530

    builded build file for deploy on firebase and also fix the bug of liveChat

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -3,6 +3,7 @@ import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessages } from "../utils/chatSlice";
 import { generate } from "../utils/helper";
+
 const LiveChat = () => {
   const dispatch = useDispatch();
   const chatMessage = useSelector((store) => store.chat.messages);
@@ -21,50 +22,50 @@ const LiveChat = () => {
     return () => clearInterval(i);
   }, [dispatch]);
 
-  return (
-    <div>
-      <div className="w-1/3 h-[600px] ml-2 mt-4 p-2 mr-4 border border-zinc-500 bg-slate-50 rounded-lg overflow-y-auto flex flex-col-reverse">
-        <div>
+    return (
+      <div className="w-1/3 h-[600px] ml-2 mt-2 p-2 mr-4 ">
+        <div className=" ml-2 h-[550px] mt-4 p-2 border border-zinc-500 bg-slate-50 rounded-lg  flex flex-col-reverse overflow-y-auto">
           {chatMessage.map((m, index) => (
             <ChatMessage key={index} name={m.name} message={m.message} />
           ))}
         </div>
-      </div>
-      <form
-        className="p-2 border border-black"
-        action="#"
-        onSubmit={(e) => {
-          e.preventDefault();
-          dispatch(
-            addMessages({
-              name: "Prabhat Singh",
-              message: liveMessage,
-            })
-          );
-        }}
-      >
-        <input
-          type="text"
-          placeholder="write here to chat"
-          value={liveMessage}
-          onChange={(e) => {
-            setLiveMessage(e.target.value);
+
+        <form
+          className="p-2 border border-black rounded-xl"
+          onSubmit={(e) => {
+            e.preventDefault();
+            dispatch(
+              addMessages({
+                name: "Prabhat Singh",
+                message: liveMessage,
+              })
+            );
+            setLiveMessage(""); // clear input after sending
           }}
-        />
-        <button
-          className="bg-gray-950 p-2 m-2 text-white focus:bg-white focus:text-black rounded-lg"
-          onClick={dispatch(
-            addMessages({
-              name: "Prabhat Singh",
-              message: liveMessage,
-            })
-          )}
         >
-          send
-        </button>
-      </form>
-    </div>
-  );
+          <input
+            type="text"
+            placeholder="write here to chat"
+            value={liveMessage}
+            onChange={(e) => setLiveMessage(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="bg-gray-950 p-2 m-2 text-white focus:bg-white focus:text-black rounded-lg"
+            onClick={() =>
+              dispatch(
+                addMessages({
+                  name: "Prabhat Singh",
+                  message: liveMessage,
+                })
+              )
+            }
+          >
+            send
+          </button>
+        </form>
+      </div>
+    );
 };
 
 export default LiveChat;
